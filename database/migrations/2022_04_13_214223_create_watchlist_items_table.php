2eedf24e0e12dd8e0f6b04f68246de3a6fd1d646
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-15 23:30:35 +0200

    feat: add watchlist items table migration

---
+++ database/migrations/2022_04_13_214223_create_watchlist_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('watchlist_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('watchlist_id')->references('id')->on('watchlists');
+            $table->integer('item_order');
+            $table->foreignId('item_id')->references('id')->on('items');
+            $table->double('rating');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('watchlist_items');
+    }
+};
