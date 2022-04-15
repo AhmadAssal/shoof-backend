71f50378ec1000526c403d160b726db1b0dcd750
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-15 23:36:04 +0200

    feat: create item model

---
+++ app/Models/Item.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Watchlist;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    protected $table = 'items';
+
+    public function watchlist()
+    {
+        return $this->belongsTo(Watchlist::class);
+    }
+}
