457885f1aa864ca326a32906f7e065a62cece3f4
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-21 17:39:10 +0200

    feat: create register function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,8 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
+
+use Illuminate\Support\Facades\Hash;
+
+use App\Http\Requests\RegisterRequest;
 
 class UserController extends Controller
 {
-    //
+    public function register(RegisterRequest $request)
+    {
+        $validated = $request->validated();
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => Hash::make($validated['password'])
+        ]);
+
+        $token = $user->createToken('shoof_token')->plainTextToken;
+        $response = [
+            'token' => $token,
+            'user' => $user
+        ];
+        return response($response, 201);
+    }
 }
