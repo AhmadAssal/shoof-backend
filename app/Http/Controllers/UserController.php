2a89edefbe77248c6bb5e350c9145cdf4d33a378
AhmadAssal <ahmadnaderassal@gmail.com>
2022-04-23 11:21:17 +0200

    feat: implement login and log out

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Hash;
 
 use App\Http\Requests\RegisterRequest;
 
+
 class UserController extends Controller
 {
     public function register(RegisterRequest $request)
@@ -25,6 +26,34 @@ class UserController extends Controller
             'token' => $token,
             'user' => $user
         ];
-        return response($response, 201);
+        return response()->json($response, 201);
+    }
+
+
+
+    public function login(Request $request)
+    {
+        $validated = json_decode($request->getContent());
+
+        $user = User::where("email", $validated->email)->first();
+        if (is_null($user) || !Hash::check($validated->password, $user->password))
+            return response()->json(["error" => "User credentials not found"], 401);
+        $token = $user->createToken('shoof_token')->plainTextToken;
+        $response = [
+            'token' => $token,
+            'user' => $user
+        ];
+        return response()->json($response, 200);
+    }
+
+
+    public function logout(Request $request)
+    {
+        $request->user()->tokens()->delete();
+
+        $response = [
+            "message" => "you have been logged out"
+        ];
+        return response()->json($response, 200);
     }
 }
